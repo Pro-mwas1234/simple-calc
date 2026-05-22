20f78d902432998ea2be3138f7349db9864b2692
Pro-mwas1234 <130271674+Pro-mwas1234@users.noreply.github.com>
2026-05-22 09:37:43 +0300

    Create calc.c

---
+++ calc.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main(){
+  char opt;
+  int a, b, rst;
+  printf("simple calc\n\n");
+  printf("enter first number: ");
+    scanf("%d", &a);
+  printf("chosse the oparater u want to use\n\n");
+  printf("+, -, /, *\n");
+    scanf("%c", &opt);
+  printf("enter second number: ");
+    scanf("%d", &b);
+  switch(opt){
+    case '+':
+    rst= a+b;
+
+    
+  }
+  
+
+
+
+
+
+  
+}
