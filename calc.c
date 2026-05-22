e521a43b4ef329800db25abfb3eda593b6980360
Pro-mwas1234 <130271674+Pro-mwas1234@users.noreply.github.com>
2026-05-22 11:28:23 +0300

    calc complete

--- calc.c
+++ calc.c
@@ -1,26 +1,35 @@
-#include<stdio.h>
+#include <stdio.h>
 int main(){
-  char opt;
-  int a, b, rst;
-  printf("simple calc\n\n");
-  printf("enter first number: ");
-    scanf("%d", &a);
-  printf("chosse the oparater u want to use\n\n");
-  printf("+, -, /, *\n");
-    scanf("%c", &opt);
-  printf("enter second number: ");
-    scanf("%d", &b);
-  switch(opt){
-    case '+':
-    rst= a+b;
-
-    
-  }
-  
-
-
-
-
-
-  
-}
+	//initialize the variables a b and rst
+     char opt; // hii ina symbolise option chozen by user
+     int a, b, rst;
+     printf("simple calc\n\n");
+     printf("choose the sign u wanna use\n\n(+ × ÷  -) \n ");
+     scanf("%c",&opt);//input ya sign
+     
+     //asks user for values 
+     printf("enter first number: ");
+     scanf("%d", &a);
+     
+     printf("enter second number: ");
+     scanf("%d", &b);
+     // options and hizo calculations
+     switch(opt){
+     	case '+':
+     		rst=a+b;
+     		printf("\nThe answer is : %d",rst);
+     		break;
+     	case '-':
+     		rst=a-b;
+     		printf("\n The answer is : %d",rst);	
+    	case '/':
+     		rst=a/b;
+     		printf("\nThe answer is : %d",rst);
+     		break;
+     	case '*':
+     		rst=a*b;
+     		printf("\n The answer is : %d",rst);
+     	      	
+     	}
+     return 0;
+ }
